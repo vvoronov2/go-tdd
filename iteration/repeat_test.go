d66bee1aea8229ccaa6524a8a775ffd3332529cd
vvoronov2 <valeravoronov96@gmail.com>
2022-10-23 18:42:38 +0300

    fix modules, added iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,38 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	t.Run("5 times a", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	t.Run("7 times b", func(t *testing.T) {
+		repeated := Repeat("b", 7)
+		expected := "bbbbbbb"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 25)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("x", 8)
+	fmt.Println(repeated)
+	// Output: xxxxxxxx
+}
