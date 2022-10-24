24c518bd232563602e4ba1f5c54f092850fa3fcf
vvoronov2 <valeravoronov96@gmail.com>
2022-10-24 21:14:35 +0300

    fix readme, arrays and slices

---
+++ arrays/sum.go
@@ -0,0 +1,38 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
+func Cap(cars []string) int {
+	// The length and capacity of a slice can be inspected using the built-in len and cap functions.
+	capacity := cap(cars)
+	return capacity
+}
